8ce6762334b50f29936b180c4132f3d587715413
Miguel <miguel.alatorre@gmail.com>
2010-09-13 02:02:45 -0700

    Second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlatorreDevelopment::Application.config.secret_token = 'c4e19997c3f068dd9db1f5a3992dc0ee767de4ebf054009be9f28cf761569003e2343f43f393018bf06ed93a0167d868a2d6b335f880880fd3353f87c76b4c8a'
